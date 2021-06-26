9b2b289f8e71fba6bb2a1f4a044acd68839b39c8
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-06-26 13:12:23 +0300

    Change the front page picture and add a footer

--- frontend/src/components/Etusivu.js
+++ frontend/src/components/Etusivu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from "./Footer"
 import homePagePicture from '../homePagePicture.jpg';
 
 const Etusivu = () => {
@@ -6,7 +7,8 @@ const Etusivu = () => {
     return(
     <div style={{textAlign: "center"}}>
         <h1>TREENIPÄIVÄKIRJA</h1>
-        <img src={homePagePicture} alt="stretching person" className="responsive"></img>
+        <img src={homePagePicture} alt="stretching person" className="responsive"></img><br />
+        <Footer/>
     </div>
     )
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>Etusivun kuva: <a href="https://www.freepik.com/free-photos-vectors/people">People vector created by pch.vector - www.freepik.com</a></p>
+  </div>
+);
+
+export default Footer;
